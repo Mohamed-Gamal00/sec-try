5efc8bf5b35e9bd5ed1fab2ca7462076fe9bcdc1
Mohamed-Gamal110 <gmohamed777@yahoo.com>
2021-01-11 20:13:01 +0200

    secont try

---
+++ jquery/jquery.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $(".check_box").click(function(){
+        $(".form").slideToggle();
+    });
+})
+
+
+/* popular question*/
+
+$(document).ready(function(){
+    $(".h3_button").click(function(){
+        $(".contain_box").slideToggle(1000);
+    });
+})
+
+$(document).ready(function(){
+    $(".frist_box").click(function(){
+        $(".hamada").slideToggle();
+    });
+})
+
+
+
+$(document).ready(function(){
+    $(".second_boxsecond_box").click(function(){
+        $(".hamada2").slideToggle();
+    });
+})
+
+$(document).ready(function(){
+    $(".third_box").click(function(){
+        $(".hamada3").slideToggle();
+    });
+})
+
+
+$(document).ready(function(){
+    $(".forth_box").click(function(){
+        $(".hamada4").slideToggle();
+    });
+})
+/* popular question*/
+
+
+
+
+
---
+++ jquery/jquery/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".Tenant button").click(function(){
+        $(".fristMethod").slideToggle();
+    });
+})
+
+
+$(document).ready(function(){
+    $(".Landlords button").click(function(){
+        $(".secondMethod").slideToggle();
+    });
+})
